ec8ccd729296df954a1e87b3c45ea5c5f3ab422d
Gabriel Antelo <gabriel1536@gmail.com>
2022-09-21 12:27:27 -0300

    ♻️ Refactor android package stub implementation (#6)

--- packages/file_selector/file_selector_android/android/src/main/java/io/flutter/plugins/file_selector/FileSelectorPlugin.java
+++ packages/file_selector/file_selector_android/android/src/main/java/io/flutter/plugins/file_selector/FileSelectorPlugin.java
@@ -1,53 +1,72 @@
 package io.flutter.plugins.file_selector;
 
+import android.content.Context;
+import android.util.Log;
+
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.Arrays;
+import java.util.List;
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
-import io.flutter.plugin.common.MethodCall;
-import io.flutter.plugin.common.MethodChannel;
-import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
-import io.flutter.plugin.common.MethodChannel.Result;
+import io.flutter.plugin.common.BinaryMessenger;
+import io.flutter.plugins.file_selector.Messages.FileSelectorApi;
 
 /** FileSelectorPlugin */
-public class FileSelectorPlugin implements FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native
-  /// Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine
-  /// and unregister it
-  /// when the Flutter Engine is detached from the Activity
-  private MethodChannel channel;
-  private static final String CHANNEL = "plugins.flutter.io/file_selector_android";
+public class FileSelectorPlugin implements FlutterPlugin, FileSelectorApi {
+  static final String TAG = "FileSelectorPlugin";
 
-  static final String METHOD_CALL_OPEN_FILE = "openFile";
-  static final String METHOD_CALL_GET_DIRECTORY_PATH = "getDirectoryPath";
-  static final String METHOD_CALL_OPEN_FILES = "openFiles";
-  static final String METHOD_CALL_GET_SAVE_PATH = "getSavePath";
+  public FileSelectorPlugin() {
+  }
 
-  @Override
-  public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
-    channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), CHANNEL);
-    channel.setMethodCallHandler(this);
+  private void setup(BinaryMessenger messenger, Context context) {
+    BinaryMessenger.TaskQueue taskQueue = messenger.makeBackgroundTaskQueue();
+
+    try {
+      FileSelectorApi.setup(messenger, this);
+    } catch (Exception ex) {
+      Log.e(TAG, "Received exception while setting up FileSelectorPlugin", ex);
+    }
+  }
+
+  @SuppressWarnings("deprecation")
+  public static void registerWith(io.flutter.plugin.common.PluginRegistry.Registrar registrar) {
+    FileSelectorPlugin instance = new FileSelectorPlugin();
+    instance.setup(registrar.messenger(), registrar.context());
   }
 
   @Override
-  public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
-    switch (call.method) {
-      case METHOD_CALL_OPEN_FILE:
-        throw new UnsupportedOperationException("Method 'openFile' not supported yet.");
-      case METHOD_CALL_GET_DIRECTORY_PATH:
-        throw new UnsupportedOperationException("Method 'getDirectoryPath' not supported yet.");
-      case METHOD_CALL_OPEN_FILES:
-        throw new UnsupportedOperationException("Method 'openFiles' not supported yet.");
-      case METHOD_CALL_GET_SAVE_PATH:
-        throw new UnsupportedOperationException("Method 'getSavePath' not supported yet.");
-      default:
-        throw new IllegalArgumentException("Unknown method " + call.method);
-    }
+  public void onAttachedToEngine(@NonNull FlutterPluginBinding binding) {
+    setup(binding.getBinaryMessenger(), binding.getApplicationContext());
   }
 
   @Override
   public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
-    channel.setMethodCallHandler(null);
+    FileSelectorApi.setup(binding.getBinaryMessenger(), null);
+  }
+
+  @NonNull
+  @Override
+  public List<String> startFileExplorer(@NonNull Messages.FileSelectorMethod method,
+      @NonNull Messages.SelectionOptions options, @Nullable String initialDirectory,
+      @Nullable String confirmButtonText) {
+    switch (method) {
+      case OPEN_FILE:
+        return Arrays.asList("A");
+      case GET_DIRECTORY_PATH:
+        return Arrays.asList("B");
+      case OPEN_MULTIPLE_FILES:
+        return Arrays.asList("B", "C");
+      default:
+        return Arrays.asList("A", "B", "C");
+    }
+  }
+
+  @Nullable
+  @Override
+  public String openSaveDialog(@NonNull Messages.SelectionOptions options, @Nullable String initialDirectory,
+      @Nullable String confirmButtonText, @Nullable String suggestedName) {
+    return null;
   }
 }
