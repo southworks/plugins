671e602025c0594c7dfe80e304b924a66a021df0
Juan Angel Dausa <juan.dausa@southworks.com>
2022-09-23 12:34:26 -0300

    Update file_open_dialog_wrapper.dart documentation.

--- packages/file_selector/file_selector_windows/lib/src/file_open_dialog_wrapper.dart
+++ packages/file_selector/file_selector_windows/lib/src/file_open_dialog_wrapper.dart
@@ -8,22 +8,22 @@ import 'package:win32/win32.dart';
 
 /// FileOpenDialogWrapper provides an abstraction to interact with IFileOpenDialog related methods.
 class FileOpenDialogWrapper {
-  /// Sets dialog options.
+  /// Sets the options given into an IFileOpenDialog.
   int setOptions(int options, IFileOpenDialog dialog) {
     return dialog.setOptions(options);
   }
 
-  /// Returns dialog options.
+  /// Returns the IFileOpenDialog's options.
   int getOptions(Pointer<Uint32> ptrOptions, IFileOpenDialog dialog) {
     return dialog.getOptions(ptrOptions);
   }
 
-  /// Set confirmation button text on dialog.
+  /// Set confirmation button text on an IFileOpenDialog.
   int setOkButtonLabel(String? confirmationText, IFileOpenDialog dialog) {
     return dialog.setOkButtonLabel(TEXT(confirmationText ?? 'Pick'));
   }
 
-  /// Sets allowed file type extensions.
+  /// Sets the allowed file type extensions in an IFileOpenDialog.
   int setFileTypes(
       Map<String, String> filterSpecification, IFileOpenDialog dialog) {
     final Pointer<COMDLG_FILTERSPEC> registerFilterSpecification =
@@ -41,46 +41,52 @@ class FileOpenDialogWrapper {
         filterSpecification.length, registerFilterSpecification);
   }
 
-  /// Shows a dialog.
+  /// Shows an IFileOpenDialog using the given owner.
   int show(int hwndOwner, IFileOpenDialog dialog) {
     return dialog.show(hwndOwner);
   }
 
-  /// Release a dialog.
+  /// Release an IFileOpenDialog.
   int release(IFileOpenDialog dialog) {
     return dialog.release();
   }
 
-  /// Return a result from a dialog.
+  /// Return a result from an IFileOpenDialog.
   int getResult(
       Pointer<Pointer<COMObject>> ptrCOMObject, IFileOpenDialog dialog) {
     return dialog.getResult(ptrCOMObject);
   }
 
-  /// Return results from a dialog, this should be used when selecting multiple items.
+  /// Return results from an IFileOpenDialog, this should be used when selecting multiple items.
   int getResults(
       Pointer<Pointer<COMObject>> ptrCOMObject, IFileOpenDialog dialog) {
     return dialog.getResults(ptrCOMObject);
   }
 
-  /// Sets the initial directory for a dialog.
+  /// Sets the initial directory for an IFileOpenDialog.
   int setFolder(Pointer<Pointer<COMObject>> ptrPath, IFileOpenDialog dialog) {
     return dialog.setFolder(ptrPath.value);
   }
 
-  /// Sets the file name for a dialog.
+  /// Sets the suggested file name for an IFileOpenDialog.
   int setFileName(String suggestedFileName, IFileOpenDialog dialog) {
     return dialog.setFileName(TEXT(suggestedFileName));
   }
 
-  /// Creates item from a given initial directory. This throws if the directory does not exist.
+  /// Creates and [initializes](https://learn.microsoft.com/en-us/windows/win32/api/shobjidl_core/nf-shobjidl_core-shcreateitemfromparsingname) a Shell item object from a parsing name.
+  /// If the directory doesn't exist it will return an error result.
   int createItemFromParsingName(String initialDirectory, Pointer<GUID> ptrGuid,
       Pointer<Pointer<NativeType>> ptrPath) {
     return SHCreateItemFromParsingName(
         TEXT(initialDirectory), nullptr, ptrGuid, ptrPath);
   }
 
-  /// Initilaize the COM library.
+  /// Initilaize the COM library with the internal [CoInitializeEx](https://pub.dev/documentation/win32/latest/winrt/CoInitializeEx.html) method.
+  /// It uses the following parameters:
+  /// pvReserved (Pointer): nullptr
+  /// dwCoInit (int):  COINIT_APARTMENTTHREADED | COINIT_DISABLE_OLE1DDE
+  /// COINIT_APARTMENTTHREADED: Initializes the thread for apartment-threaded object concurrency.
+  /// COINIT_DISABLE_OLE1DDE: Disables Dynamic Data Exchange for Ole1 [support](https://learn.microsoft.com/en-us/windows/win32/learnwin32/initializing-the-com-library).
   int coInitializeEx() {
     return CoInitializeEx(
         nullptr, COINIT_APARTMENTTHREADED | COINIT_DISABLE_OLE1DDE);
@@ -91,7 +97,7 @@ class FileOpenDialogWrapper {
     return FileOpenDialog.createInstance();
   }
 
-  /// Close the COM library.
+  /// Closes the COM library on the current thread, unloads all DLLs loaded by the thread, frees any other resources that the thread maintains, and forces all RPC connections on the thread to close.
   void coUninitialize() {
     CoUninitialize();
   }
