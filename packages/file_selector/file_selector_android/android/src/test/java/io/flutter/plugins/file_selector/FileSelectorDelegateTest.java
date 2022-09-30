8966ea8c834e4e423146d3393b05f3b4ee5e135e
Gabriel Antelo <gabriel1536@gmail.com>
2022-09-30 11:49:31 -0300

    Add file selector delegate tests

--- packages/file_selector/file_selector_android/android/src/test/java/io/flutter/plugins/file_selector/FileSelectorDelegateTest.java
+++ packages/file_selector/file_selector_android/android/src/test/java/io/flutter/plugins/file_selector/FileSelectorDelegateTest.java
@@ -7,7 +7,10 @@ package io.flutter.plugins.file_selector;
 import static io.flutter.plugins.file_selector.FileSelectorPlugin.METHOD_GET_DIRECTORY_PATH;
 import static io.flutter.plugins.file_selector.FileSelectorPlugin.METHOD_GET_SAVE_PATH;
 import static io.flutter.plugins.file_selector.TestHelpers.buildMethodCall;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
@@ -17,10 +20,14 @@ import android.content.Intent;
 import android.net.Uri;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
 
 public class FileSelectorDelegateTest {
   @Mock Activity mockActivity;
@@ -29,12 +36,27 @@ public class FileSelectorDelegateTest {
 
   @Mock Uri mockUri;
 
+  @Mock PathUtils mockPathUtils;
+  @Spy FileSelectorDelegate spyFileSelectorDelegate;
+
   @Before
   public void setUp() {
     MockitoAnnotations.openMocks(this);
 
-    mockUri = mock(Uri.class);
+    //    spyFileSelectorDelegate.cacheFolder = fakeFolder;
+
+    spyFileSelectorDelegate = spy(new FileSelectorDelegate(mockActivity));
     when(mockIntent.getData()).thenReturn(mockUri);
+  }
+
+  @After
+  public void tearDown() {
+    reset(mockUri);
+    reset(mockActivity);
+    reset(mockIntent);
+    reset(mockPathUtils);
+    reset(mockResult);
+    reset(spyFileSelectorDelegate);
   }
 
   @Test
@@ -82,8 +104,64 @@ public class FileSelectorDelegateTest {
     verifyNoMoreInteractions(mockResult);
   }
 
-  private FileSelectorDelegate createDelegate() {
-    return new FileSelectorDelegate(mockActivity, null, null);
+  public void getSavePath_WhenItIsCalled_launchGetDirectoryPath_NullArguments() {
+    MethodCall call = buildMethodCall(METHOD_GET_SAVE_PATH, null, null, null);
+    spyFileSelectorDelegate = spy(new FileSelectorDelegate(mockActivity));
+
+    doAnswer(
+            (invocation) -> {
+              return null;
+            })
+        .when(spyFileSelectorDelegate)
+        .launchGetSavePath(null, null);
+
+    spyFileSelectorDelegate.getSavePath(call, mockResult);
+
+    verify(spyFileSelectorDelegate).launchGetSavePath(null, null);
+  }
+
+  @Test
+  public void getSavePath_WhenItIsCalled_launchGetDirectoryPath_WithInitialDirAndSuggestedName() {
+    MethodCall call = buildMethodCall(METHOD_GET_SAVE_PATH, "testDir", null, "testName");
+    spyFileSelectorDelegate = spy(new FileSelectorDelegate(mockActivity));
+
+    doAnswer(
+            (invocation) -> {
+              return null;
+            })
+        .when(spyFileSelectorDelegate)
+        .launchGetSavePath("testDir", "testName");
+
+    spyFileSelectorDelegate.getSavePath(call, mockResult);
+
+    verify(spyFileSelectorDelegate).launchGetSavePath("testDir", "testName");
+  }
+
+  @Test
+  public void onActivityResult_WhenGetSavePath_InvokesHandleGetSavePathResult() {
+    doAnswer(
+            (invocation) -> {
+              return null;
+            })
+        .when(spyFileSelectorDelegate)
+        .handleGetSavePathResult(Activity.RESULT_OK, mockIntent);
+
+    spyFileSelectorDelegate.onActivityResult(
+        FileSelectorDelegate.REQUEST_CODE_GET_SAVE_PATH, Activity.RESULT_OK, mockIntent);
+
+    verify(spyFileSelectorDelegate).handleGetSavePathResult(Activity.RESULT_OK, mockIntent);
+  }
+
+  @Test
+  public void handleGetSavePathResult_WhenItIsCalled_InvokesGetSavePathUriFromPathUtils() {
+    MethodCall call = buildMethodCall(METHOD_GET_SAVE_PATH, null, null, null);
+    FileSelectorDelegate delegate = createDelegateWithPendingResultAndMethodCall(call);
+
+    try (MockedStatic<PathUtils> mockedPathUtils = Mockito.mockStatic(PathUtils.class)) {
+      delegate.handleGetSavePathResult(Activity.RESULT_OK, mockIntent);
+
+      mockedPathUtils.verify(() -> PathUtils.getSavePathUri(mockUri, mockActivity), times(1));
+    }
   }
 
   private FileSelectorDelegate createDelegateWithPendingResultAndMethodCall(MethodCall call) {
